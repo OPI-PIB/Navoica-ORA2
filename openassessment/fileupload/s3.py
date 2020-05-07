747de8ad755048d9dd90ee172c439e09403d4cd1
Aleksander Majda <nazgut@gmail.com>
2020-05-07 16:07:50 +0200

    fix put

--- openassessment/fileupload/s3.py
+++ openassessment/fileupload/s3.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import logging
 import os
+import hashlib
 from django.conf import settings
 from boto.s3.key import Key
 from .exceptions import FileUploadInternalError
@@ -9,6 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from .backends.s3 import _connect_to_s3
 from django.shortcuts import HttpResponse
+from django.utils import timezone
 
 @login_required()
 @require_http_methods(["PUT"])
@@ -18,8 +20,9 @@ def s3_storage(request, key):
     """
     conn = _connect_to_s3()
     conn.auth_region_name = 'eu-frankfurt-1'
-    file = open('unnamed.jpg', 'r+')
-    filename = 'unnamed.jpg'
+    content, metadata = get_content_metadata(request)
+    file = content
+    #filename = 'unnamed2.jpg'
     try:
         size = os.fstat(file.fileno()).st_size
     except:
@@ -40,3 +43,20 @@ def s3_storage(request, key):
     if size == sent:
         return HttpResponse()
     return False
+
+def get_content_metadata(request):
+    """
+    Read the content and metadata associated to an HttpRequest.
+
+    Returns:
+        request body (str)
+        request metadata (dict)
+    """
+
+    metadata = {
+        "Content-Type": request.META["CONTENT_TYPE"],
+        "Date": str(timezone.now()),
+        "Content-MD5": hashlib.md5(request.body).hexdigest(),
+        "Content-Length": request.META["CONTENT_LENGTH"],
+    }
+    return request.body, metadata
